916ebdd7f90f356dab1213f56330ef87ea07e067
Bakrifly <bakri.hmouda@gmail.com>
2021-12-12 21:42:44 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -4,14 +4,14 @@ SELECT "Name" FROM students;
 
 # 2. Get all the data of students above 30 years old 
 
-SELECT * FROM students WHERE Age > 30;
+SELECT  FROM students WHERE Age > 30;
 
 # 3. Get the names of the females who are 30 years old 
 
 SELECT Name FROM students
 WHERE Gender = "F" AND Age > 30; # !!There are no result for female who are 30 so I used Greater Than 30 instead.
 
-# 4. Get the number of Points of **Alex** 
+# 4. Get the number of Points of Alex 
 
 SELECT Points FROM students
 WHERE Name = "Alex";
@@ -20,14 +20,42 @@ WHERE Name = "Alex";
 
 INSERT INTO students VALUES(7, 'Bakri', '31', 'M', '500');
 
-# 6. Increase the points of **Basma** because she solved a new exercise 
+# 6. Increase the points of Basma because she solved a new exercise 
 
 UPDATE students
 SET Points = 500
 WHERE name = 'Basma';
 
-# 7. Decrease the points of **Alex** because he's late today 8.
+# 7. Decrease the points of Alex because he's late today 8.
 
 UPDATE students
 SET Points = 150
 WHERE name = 'Alex';
+
+
+#######################################
+
+# Creating Table
+
+CREATE TABLE "Graduates" (
+"ID" INTEGER,
+"Name" TEXT NOT NULL UNIQUE,
+"Age" INTEGER,
+"Gender" TEXT,
+"Points" INTEGER,
+"Graduation" DATE,
+PRIMARY KEY ("ID" AUTOINCREMENT)
+);
+
+
+# Copy Layal's data from students to graduates
+
+INSERT INTO Graduates (Name, Age, Gender, Points, Graduation)
+SELECT Name, Age, Gender, Points, '08/10/2018' FROM students
+WHERE ID = 4;
+
+
+# Remove Layal's record from students
+
+DELETE FROM students
+WHERE ID = 4;
